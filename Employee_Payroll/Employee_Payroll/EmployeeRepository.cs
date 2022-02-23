eb6159b775d058b3956ee071cf1c26463d10d9f6
yogesh1232 <yogeshkusakiya77@gmail.com>
2022-02-23 18:23:50 +0530

    [Yogesh] Adding program for adding emp details in DB

--- Employee_Payroll/Employee_Payroll/EmployeeRepository.cs
+++ Employee_Payroll/Employee_Payroll/EmployeeRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -13,50 +14,92 @@ namespace Employee_Payroll
         readonly SqlConnection connection = new SqlConnection(connectionString);
         public void GetEmployeeRecords()
         {
-            try
-            {
-                EmployeeModel model = new EmployeeModel();
-                using (this.connection)
+                try
                 {
-                    string query = @"SELECT * from employee_payroll;";
-                    SqlCommand cmd = new SqlCommand(query, connection);
-                    this.connection.Open();
-                    SqlDataReader sdr = cmd.ExecuteReader();
-                    if (sdr.HasRows)
+                    EmployeeModel model = new EmployeeModel();
+                    using (this.connection)
                     {
-                        while (sdr.Read())
+                        string query = @"SELECT * from employee_payroll;";
+                        SqlCommand cmd = new SqlCommand(query, connection);
+                        this.connection.Open();
+                        SqlDataReader sdr = cmd.ExecuteReader();
+                        if (sdr.HasRows)
                         {
-                            model.EmployeeId = sdr.GetInt32(0);
-                            model.EmployeeName = sdr.GetString(1);
-                            model.BasicPay = sdr.GetInt32(2);
-                            model.StartDate = sdr.GetDateTime(3);
-                            model.Gender = sdr.GetString(4);
-                            model.PhoneNumber = sdr.GetInt64(5);
-                            model.Address = sdr.GetString(6);
-                            model.Department = sdr.GetString(7);
-                            model.Deductions = sdr.GetInt32(8);
-                            model.TaxablePay = sdr.GetInt32(9);
-                            model.IncomeTax = sdr.GetInt32(10);
-                            model.NetPay = sdr.GetInt32(11);
-                            //Print Record on Console
-                            Console.WriteLine("{0},{1},{2},{3},{4},{5}", model.EmployeeId, model.EmployeeName, model.Gender, model.Department, model.Address, model.NetPay);
-                            Console.WriteLine("\n");
+                            while (sdr.Read())
+                            {
+                                model.EmployeeId = sdr.GetInt32(0);
+                                model.EmployeeName = sdr.GetString(1);
+                                model.BasicPay = sdr.GetInt32(2);
+                                model.StartDate = sdr.GetDateTime(3);
+                                model.Gender = Convert.ToChar(sdr.GetString(4));
+                                model.PhoneNumber = sdr.GetInt64(5);
+                                model.Address = sdr.GetString(6);
+                                model.Department = sdr.GetString(7);
+                                model.Deductions = sdr.GetInt32(8);
+                                model.TaxablePay = sdr.GetInt32(9);
+                                model.IncomeTax = sdr.GetInt32(10);
+                                model.NetPay = sdr.GetInt32(11);
+                                //Print Record on Console
+                                Console.WriteLine("{0},{1},{2},{3},{4},{5}", model.EmployeeId, model.EmployeeName, model.Gender, model.Department, model.Address, model.NetPay);
+                                Console.WriteLine("\n");
+                            }
                         }
+                        else
+                            Console.WriteLine("No Records in Database");
+                        sdr.Close();
+                        this.connection.Close();
                     }
-                    else
-                        Console.WriteLine("No Records in Database");
-                    sdr.Close();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+                finally
+                {
                     this.connection.Close();
                 }
             }
-            catch (Exception ex)
+            public void AddEmployee(EmployeeModel model)
             {
-                throw new Exception(ex.Message);
-            }
-            finally
-            {
-                this.connection.Close();
+                try
+                {
+                    using (this.connection)
+                    {
+                        SqlCommand command = new SqlCommand("spAddEmployeeDetails", this.connection);
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@name", model.EmployeeName);
+                        command.Parameters.AddWithValue("@basic_Pay", model.BasicPay);
+                        command.Parameters.AddWithValue("@start_date", model.StartDate);
+                        command.Parameters.AddWithValue("@Gender", model.Gender);
+                        command.Parameters.AddWithValue("@phonenumber", model.PhoneNumber);
+                        command.Parameters.AddWithValue("@address", model.Address);
+                        command.Parameters.AddWithValue("@department", model.Department);
+                        command.Parameters.AddWithValue("@Deductions", model.Deductions);
+                        command.Parameters.AddWithValue("@taxable_pay", model.TaxablePay);
+                        command.Parameters.AddWithValue("@income_tax", model.IncomeTax);
+                        command.Parameters.AddWithValue("@net_pay", model.NetPay);
+
+
+                        connection.Open();
+                        var result = command.ExecuteNonQuery();
+                        connection.Close();
+                        this.connection.Close();
+                        if (result != 0)
+                            Console.WriteLine("Data inserted in DataBase");
+                        else
+                            Console.WriteLine(result);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
             }
         }
-    }
 }
+
--- Employee_Payroll/Employee_Payroll/Program.cs
+++ Employee_Payroll/Employee_Payroll/Program.cs
@@ -1,6 +1,38 @@
-﻿using Employee_Payroll;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-EmployeeRepository employeeRepository = new EmployeeRepository();
-employeeRepository.GetEmployeeRecords();
-Console.WriteLine("Welcome to Employee Payroll program");
-Console.ReadLine();
+namespace Employee_Payroll
+{
+    internal class Program
+    {
+        public static void EmployeePayroll()
+        {
+            EmployeeRepository employeeRepository = new EmployeeRepository();
+            //employeeRepository.GetEmployeeRecords();
+            EmployeeModel Model = new EmployeeModel();
+            Model.EmployeeId = 2;
+            Model.EmployeeName = "Jayesh";
+            Model.PhoneNumber = 34543787519;
+            Model.Address = "Bagpat";
+            Model.Department = "Engineer";
+            Model.Gender = 'M';
+            Model.BasicPay = 20000;
+            Model.Deductions = 100;
+            Model.TaxablePay = 19900;
+            Model.IncomeTax = 0;
+            Model.StartDate = DateTime.Now;
+            Model.NetPay = 19900;
+
+            employeeRepository.AddEmployee(Model);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll program");
+            EmployeePayroll();
+            Console.ReadLine();
+        }
+    }
+}
